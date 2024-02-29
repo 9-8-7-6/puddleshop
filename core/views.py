20eb54686190a9f56ea99d9ce723c1e6a6f3c69b
vito <ac1246897@gmail.com>
2024-02-29 13:48:30 +0800

    first commit

---
+++ core/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+from item.models import Category, Item
+
+from .forms import SignupForm
+
+def index(request):
+    items = Item.objects.filter(is_sold=False)[0:6]
+    Categories = Category.objects.all()
+    
+    return render(request, 'core/index.html',{
+        'categories': Categories,
+        'items': items,
+    })
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+def signup(request):
+    form = SignupForm()
+    
+    return render(request, 'core/signup.html',{
+         'form': form
+    })
