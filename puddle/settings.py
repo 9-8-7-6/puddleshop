94163882d58b68c278064de9db171f13065ea662
vito <ac1246897@gmail.com>
2024-03-03 00:11:00 +0800

    key

--- puddle/settings.py
+++ puddle/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-3&gt67evacw-!nd$w*&8n#_zdi@38jo47y(vjya!m*bmmz-pdq'
+SECRET_KEY = 'django-insecure-3&gt67evacw-!nd$w*&8n#_zdi@38jo47y(vjya!m*bmmz-pdq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
